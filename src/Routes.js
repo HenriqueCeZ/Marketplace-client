5347bab18fb29e53bd3a90c16ea5aa615db55533
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-03 08:07:49 -0300

    feat: create routes start and import router-dom

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+export default function Routes(){
+    return(
+        <Switch>
+             <Route exact path="/">
+                    <Home/>
+                 </Route>   
+                 <Route exact path="/about">
+                    <About/>
+                 </Route>   
+
+        </Switch>
+    )
+}
