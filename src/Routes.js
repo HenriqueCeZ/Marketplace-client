2621a3278ff5d4e917a48c99683d2bf60d8aaf44
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-04 00:34:49 -0300

    feat: add route not found

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch,Route } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
+import NotFound from './pages/notFound'
 export default function Routes(){
     return(
         <Switch>
@@ -10,8 +11,10 @@ export default function Routes(){
                  </Route>   
                  <Route exact path="/about">
                     <About/>
-                 </Route>   
-
+                 </Route>
+                 <Route> 
+                     <NotFound />  
+                </Route>
         </Switch>
     )
 }
